caa3772803068e95644ce16255758334784d3a45
Student <student@Administrators-Mac-mini.local>
2026-04-02 12:54:11 +0300

    Ül3 Andmete pärimine Meals.js komponendist

--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,6 +1,27 @@
-
+import { useEffect } from "react";
 
 const Meals = () => {
+    useEffect(() => {
+        const fetchMeals = async () => {
+
+            try {
+                const response = await fetch("http://localhost:3001/meals");
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch meals data.");
+                }
+
+                const data = await response.json();
+                console.log("Meals data from backend:", data);
+
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchMeals();
+    }, []);
+
     return (
         <ul id="meals">
             { 
