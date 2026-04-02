488d73a267854b99954adc9862804fe0aebd843e
Student <student@Administrators-Mac-mini.local>
2026-04-02 13:27:13 +0300

    Ül5 valuuta andmete kuvamine

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -5,7 +5,9 @@ const MealItem = (props) => {
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
                     <h3 className="meal-item-name">{props.meal.name}</h3>
-                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-price">
+                        {new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR"}).format(props.meal.price)}
+                    </p>
                     <p className="meal-item-description">{props.meal.description}</p>
                 </div>
                 <p className="meal-item-actions">
