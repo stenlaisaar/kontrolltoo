f582f6c382f5f75cd9bc4d3159f731b8fb136013
Student <student@Administrators-Mac-mini.local>
2026-04-02 12:07:29 +0300

    Ül2 andmete pärimine

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/meals", async (req, res) => {
-  const meals = "[]" // data should be read from file
+  const meals = await fs.readFile(path.join(__dirname, "data", "meals.json"), "utf8");
   res.json(JSON.parse(meals));
 });
 
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,3 +1,5 @@
+
+
 const Meals = () => {
     return (
         <ul id="meals">
