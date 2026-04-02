a904276be99371e92a841ab9c8537142ac1602d3
Student <student@Administrators-Mac-mini.local>
2026-04-02 13:18:53 +0300

    Ül4 kuva saadud andmed MealItem komponendi abil

--- src/components/MealItem.js
+++ src/components/MealItem.js
@@ -1,15 +1,15 @@
 const MealItem = (props) => {
     return (
-        <li>
+        <li className="meal-item">
             <article>
                 <img src={require(`../assets/${props.meal.image}`)} alt={props.meal.name}/>
                 <div>
-                    <h3>{props.meal.name}</h3>
-                    <p>props.meal.price</p>
-                    <p>{props.meal.description}</p>
+                    <h3 className="meal-item-name">{props.meal.name}</h3>
+                    <p className="meal-item-price">{props.meal.price}</p>
+                    <p className="meal-item-description">{props.meal.description}</p>
                 </div>
-                <p>
-                    <button >Add to Cart</button>
+                <p className="meal-item-actions">
+                    <button className="button">Add to Cart</button>
                 </p>
             </article>
         </li>
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -1,6 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import MealItem from "./MealItem";
 
 const Meals = () => {
+    const [meals, setMeals] = useState([]);
+
     useEffect(() => {
         const fetchMeals = async () => {
 
@@ -13,6 +16,7 @@ const Meals = () => {
 
                 const data = await response.json();
                 console.log("Meals data from backend:", data);
+                setMeals(data);
 
             } catch (error) {
                 console.error(error);
@@ -23,10 +27,12 @@ const Meals = () => {
     }, []);
 
     return (
+
         <ul id="meals">
-            { 
-                // list of meals
-            }
+            {meals.map((meal) => (
+                <MealItem key={meal.id} meal={meal} />
+            ))}
+
         </ul>
     )
 }
